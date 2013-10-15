59dae591d5ab25e7d7ec562b0419db8985144aa6
wmunn <munn411@gmail.com>
2013-10-15 10:27:16 -0400

    Causing test to fail

--- MyMusicApp/MyMusicApp.Tests/Controllers/HomeControllerTest.cs
+++ MyMusicApp/MyMusicApp.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace MyMusicApp.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual(4,5);
         }
 
         [TestMethod]
