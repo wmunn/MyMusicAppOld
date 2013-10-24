a1c63da6be734135a86d64cfb2f05dc426262b82
wmunn <munn411@gmail.com>
2013-10-24 12:42:49 -0400

    Allows basic create, update and delete for Artists.

---
+++ MyMusicApp/MyMusicApp/Controllers/ArtistsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMusicApp.Models;
+
+namespace MyMusicApp.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private ArtistDBContext db = new ArtistDBContext();
+
+        //
+        // GET: /Artists/
+
+        public ActionResult Index()
+        {
+            return View(db.Artists.ToList());
+        }
+
+        //
+        // GET: /Artists/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        //
+        // GET: /Artists/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Artists/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Artists.Add(artist);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(artist);
+        }
+
+        //
+        // GET: /Artists/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        //
+        // POST: /Artists/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artist);
+        }
+
+        //
+        // GET: /Artists/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        //
+        // POST: /Artists/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            db.Artists.Remove(artist);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyMusicApp/MyMusicApp/Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MyMusicApp.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ArtistDBContext : DbContext
+    {
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
